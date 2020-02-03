09fab4748f8932a8ed5c24a4ca2cce26c47adaf1
Vladiablo <vladiablo4@gmail.com>
2020-02-03 13:08:27 +0300

    Added logger and additional events

---
+++ WholesaleBase/Logger.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WholesaleBase
+{
+    public class Logger
+    {
+        protected TextWriter output;
+
+        public Logger(TextWriter output)
+        {
+            this.output = output;
+        }
+        
+        public virtual void WriteLine(string message)
+        {
+            output.WriteLine(message);
+        }
+    }
+
+    public class LoadLogger
+    {
+        LoadManager loadManager;
+        Logger logger;
+
+        public LoadLogger(LoadManager loadManager, Logger logger)
+        {
+            this.loadManager = loadManager;
+            this.logger = logger;
+            loadManager.DidStartLoad += LoadStarted;
+            loadManager.DidEndLoad += LoadFinished;
+            loadManager.ObjectDidLoad += ObjectLoaded;
+        }
+
+        private void ObjectLoaded(object sender, IReadableObject e)
+        {
+            logger.WriteLine($"{DateTime.Now.ToString()} Object loaded: {e.ToString()}");
+        }
+
+        private void LoadStarted(object sender, FileInfo e)
+        {
+            logger.WriteLine($"{DateTime.Now.ToString()} Loading started from: {e.FullName}");
+        }
+
+        private void LoadFinished(object sender, FileInfo e)
+        {
+            logger.WriteLine($"{DateTime.Now.ToString()} Loading from {e.FullName} finished.");
+        }
+    }
+}
